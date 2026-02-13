52cebc38955fcf2cd0786f17bf4bfd82408582fb
Lautaro Cabrier <lautaro_cabrier@epam.com>
2026-02-13 14:06:53 -0300

    solved the edge problem

--- src/main/java/com/epam/training/student/framework/driver/DriverManager.java
+++ src/main/java/com/epam/training/student/framework/driver/DriverManager.java
@@ -1,8 +1,9 @@
 package com.epam.training.student.framework.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+
 
 import java.time.Duration;
 
@@ -10,18 +11,24 @@ public final class DriverManager {
 
     private DriverManager() {}
 
-    private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
         return DRIVER.get();
     }
 
     public static void startDriver(String browser) {
-        WebDriver driver = switch (browser.toLowerCase()) {
-            case "chrome" -> new ChromeDriver();
-            case "edge" -> new EdgeDriver();
-            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
-        };
+        WebDriver driver;
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new org.openqa.selenium.edge.EdgeDriver();
+
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
 
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); // explicit waits only
